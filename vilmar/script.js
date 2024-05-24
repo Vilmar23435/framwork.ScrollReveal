e71b9ca704ebfb3cf53dab816a70957d40739451
Vilmar23435 <168141597+Vilmar23435@users.noreply.github.com>
2024-05-24 19:48:01 -0300

    carregando primeira versao
    index.html
    style.css
    script.js

---
+++ vilmar/script.js
@@ -0,0 +1,21 @@
+window.sr = ScrollReveal({
+    reset:true
+});
+
+
+sr.reveal('.area-1', {
+    duration: 2000,
+    rotate: {x:80, y:80, z:0}
+}) ,
+
+
+sr.reveal('.area-2', {
+    duration: 2000,
+    rotate: {x:100, y:0, z:800}
+}) ,
+
+
+sr.reveal('.area-3', {
+    duration: 2000,
+    rotate: {x:100, y:0, z:0}
+}) 
